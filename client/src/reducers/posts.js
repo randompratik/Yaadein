8d489b4a08203ff9c7a75a17bd15892cabcedce7
Pratik <randompratik1999@gmail.com>
2022-06-07 16:52:41 +0530

    Action for creating post made and was dispatched

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,5 +1,5 @@
 export default (posts = [], action) => {
   if (action.type === "FETCH-ALL") return posts;
-  else if (action.type === "CREATE") return posts;
+  else if (action.type === "CREATE") return [...posts, action.payload];
   else return posts;
 };
