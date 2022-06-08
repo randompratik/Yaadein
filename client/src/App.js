b081e33e5b9ae1d1e87c3ab9ad42192bb06eff7b
Pratik <randompratik1999@gmail.com>
2022-06-08 21:35:01 +0530

    the updation property of posts was created

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, AppBar, Typography, Grid, Grow } from "@material-ui/core";
 import yaadein from "./images/yaadein.png";
 import Form from "./components/forms/form.js";
@@ -6,12 +6,17 @@ import Posts from "./components/posts/posts.js";
 import useStyle from "./styles";
 import { useDispatch } from "react-redux";
 import { getPosts } from "./actions/posts";
+
 const App = () => {
   const dispatch = useDispatch();
+
   const classes = useStyle();
+
+const [currentId, setCurrentId]= useState(null);
+
   useEffect(() => {
     dispatch(getPosts());
-  }, [dispatch]);
+  },[dispatch, currentId]);
   return (
     <Container maxidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
@@ -35,10 +40,10 @@ const App = () => {
           >
             {/* xs==xtra small, sm==small-medium */}
             <Grid item xs={12} sm={7}>
-              <Posts />
+              <Posts setCurrentId={setCurrentId} />
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Form />
+              <Form  currentId= {currentId} setCurrentId={setCurrentId}/>
             </Grid>
           </Grid>
         </Container>
