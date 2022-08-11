2a973e7bfe155969acd75e9371d9ea845567fd1b
randompratik <78574105+randompratik@users.noreply.github.com>
2022-08-11 12:13:09 +0530

    Delete server directory

--- server/routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { signup,signin } from '../controllers/user.js';
-
-const router=express.Router();
-
-router.post('/signin',signin);
-router.post('/signup',signup);
-
-export default router; 
